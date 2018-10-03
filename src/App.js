6822748ddb698c86cd1530c9f78093d2f9435dd4
Akshay Sharma <contact@imakshay.com>
2018-10-03 14:32:15 +0530

    simple login UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => (
+    <Router>
+        <div>
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/callback" component={ProfilePage} />
+        </div>
+    </Router>
+);
+
+class ProfilePage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Hello Logged in User</h1>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        );
+    }
+}
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <a href="https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=profile&response_type=code&client_id=770189468882-6333q2lrp5rbvajr91f6ebk1g9iqib9r.apps.googleusercontent.com&redirect_uri=http://localhost:3000/callback">
+                        Google Login
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
